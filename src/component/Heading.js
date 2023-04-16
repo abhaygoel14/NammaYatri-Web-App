1275951aeb9fc79e98e579460f27c3f3c2a12418
Abhay <abhay.goel14@gmil.com>
2023-04-16 16:33:09 +0530

    Injected React into HTML and parcel for igniting App

---
+++ src/component/Heading.js
@@ -0,0 +1,11 @@
+export const myName = <h1>Abhay Goel</h1>;
+
+const Heading = () => {
+  return (
+    <>
+      <h1 className="header">Hello welcome to Nammi Yatra</h1>
+    </>
+  );
+};
+
+export default Heading;
