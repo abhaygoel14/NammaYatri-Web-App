055edac419fa007a32ed8a00e912a6b1e6ad5bfc
Abhay <abhay.goel14@gmil.com>
2023-04-26 17:53:49 +0530

    Rendering nextjs route

---
+++ namma-yatri-frontend/src/pages/about/abhay.tsx
@@ -0,0 +1,11 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function Home() {
+  return (
+    <>
+      <h1> Hello this is Abhay </h1>
+    </>
+  );
+}
