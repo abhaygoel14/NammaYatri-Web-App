7e3afd01711dcd43eaf172865ded813970099d10
Abhay <abhay.goel14@gmil.com>
2023-04-29 17:29:39 +0530

    Removed unecessary files

--- namma-yatri-frontend/src/Store/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { RootState, AppDispatch } from "./";
-
-export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector;
-export const useAppDispatch = () => useDispatch<AppDispatch>();
--- namma-yatri-frontend/src/pages/_app.tsx
+++ namma-yatri-frontend/src/pages/_app.tsx
@@ -2,16 +2,12 @@ import React, { Suspense } from "react";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Loading from "@/component/Loading";
-import { Provider } from "react-redux";
-import { store } from "../Store";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Suspense fallback={null}>
       <Loading>
-        <Provider store={store}>
-          <Component {...pageProps} />
-        </Provider>
+        <Component {...pageProps} />
       </Loading>
     </Suspense>
   );
