6cb01650784ae77e4259cccb0a14ed6d8eb4e484
Abhay <abhay.goel14@gmil.com>
2023-04-30 19:42:18 +0530

    Added SEO

--- namma-yatri-frontend/src/pages/_document.tsx
+++ namma-yatri-frontend/src/pages/_document.tsx
@@ -7,9 +7,12 @@ export default function Document() {
         <meta property="og:title" content="NammaYatri" />
         <meta
           property="og:description"
-          content="Add your og:description here"
+          content="web-app for booking auto drivers"
         />
-        <meta property="og:image" content="Add your og:image here" />
+        <meta
+          property="og:image"
+          content="https://nammayatri.in/logos/nammaYatrilogo.svg"
+        />
         <title>Namma Yatri</title>
       </Head>
       <body>
